421ac0cf0612cf05e1f4e18e7d67795187e37683
Nguyen Anh Tien <tienna@vietis.vn>
2020-06-16 17:30:13 +0700

    css and do admin

--- admin/admin.js
+++ admin/admin.js
@@ -11,13 +11,69 @@ class Quiz {
   }
 }
 
+//* load when start 
 //list created question
+let _quizQuantity = 0;
 ipcRenderer.send('loadAllQues-mes');
-ipcRenderer.once('loadAllQues-rep', (event, questions) => {
-  console.log(questions);
-
-})
-//event dom 
+ipcRenderer.once('loadAllQues-rep', (event, quizzes) => {
+  _quizQuantity = quizzes.length;
+  createdQuiz_div = document.querySelector('.view-div-all');
+  quizzes.forEach((quiz, i) => {
+    createdQuiz_div.innerHTML += `
+    <div class="view-div">
+      <span class="div-index">#${i}</span>
+      <button class="btn-edit btn-linear" id="edit-${i}">Edit 🛠</button>
+      <div class="ques-div-all"><textarea readonly cols="40" rows="3">${quiz.question}</textarea>
+      </div>
+      <div class="opt-div-all" id="opts_${i}">
+  
+      </div>
+      <div class="ans-div">
+        Answer: <input type="text" value="2" readonly>
+      </div>
+      <button class="btn-delete"></button>
+    </div>
+    `;
+    quiz.options.forEach((opt) => {
+      document.querySelector(`#opts_${i}`).innerHTML += `
+      <input type="text" value="${opt}" readonly>
+      `;
+    });
+  });
+  for (let i = 0; i < quizzes.length; i++) {
+    let btnEdit = document.querySelector(`#edit-${i}`);
+    console.log(btnEdit);
+    document.querySelector(`#edit-${i}`).addEventListener('click', e => {
+      e.preventDefault();
+      console.log(`a`);
+      let ques = document.querySelector('textarea');
+      ques.readOnly = false;
+      let opts = [...document.querySelectorAll('input')];
+      opts.forEach(opt => {
+        opt.readOnly = false;
+      });
+      ques.focus();
+  
+      //save btn
+      const btnSave = document.createElement('button');
+      btnSave.innerText = `Save ✍`;
+      btnSave.classList.add('btn-edit', 'btn-linear');
+      document.querySelector('.view-div').replaceChild(btnSave, btnEdit);
+  
+      btnSave.addEventListener('click', () => {
+        ques.readOnly = true;
+        opts.forEach(opt => {
+          opt.readOnly = true;
+        });
+        document.querySelector('.view-div').replaceChild(btnEdit, btnSave);
+      });
+  
+      //send data to server
+      // let quizEdited = new Quiz()
+    });
+  }
+});
+//* event dom 
 let _lastType = null;
 let _lastQues = null;
 let _lastOpts = null;
@@ -25,6 +81,7 @@ let _lastAnsIds = null;
 
 let optionDiv = document.querySelector('.opt-div');
 let formCreate = document.querySelector('.form-create');
+//create div
 document.querySelector('.btn-create').addEventListener('click', e => {
   e.preventDefault();
   const quizType = document.querySelector('#type');
@@ -42,8 +99,8 @@ document.querySelector('.btn-create').addEventListener('click', e => {
     document.querySelector('.container').style.opacity = 0.3;
     _lastType = quizType.value;
 
-    //continue...    
     setupCreateForm(quizType.value, ansNum.value);
+
   }
 
 });
@@ -72,6 +129,7 @@ function setupCreateForm(type, num) {
   }
 }
 
+// form create 
 document.querySelector('.btn-close').addEventListener('click', e => {
   e.preventDefault();
   //let optionDiv = document.querySelector('.opt-div');
@@ -79,6 +137,8 @@ document.querySelector('.btn-close').addEventListener('click', e => {
   formCreate.style.display = 'none';
   document.querySelector('.container').style.opacity = 1;
 })
+
+
 
 document.querySelector('.submit-create').addEventListener('click', e => {
   e.preventDefault();
@@ -99,3 +159,8 @@ document.querySelector('.submit-create').addEventListener('click', e => {
     formCreate.style.display = 'none';
   }
 })
+// list created quiz
+//edit btn
+console.log(_quizQuantity);
+
+
